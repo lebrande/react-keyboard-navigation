bd76c3a5e359a3e8b84d85cbe5950e31a1870e59
Jakub Pusiak <jakub.pusiak@gmail.com>
2020-07-22 23:32:39 +0200

    Slider.

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,35 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { useSpatialNavigation } from "../../hooks/useSpatialNavigation";
+import "./Slider.css";
+
+const variants = {
+  initialPrev: { left: '10%', opacity: 0, scale: 0.5, x: '-50%', y: '-50%' },
+  exitPrev: { left: '10%', opacity: 0, scale: 0.5, x: '-50%', y: '-50%' },
+  prev: { left: '30%', opacity: 1, scale: 0.8, x: '-50%', y: '-50%' },
+  active: { left: '50%', opacity: 1, scale: 1, x: '-50%', y: '-50%' },
+  next: { left: '70%', opacity: 1, scale: 0.8, x: '-50%', y: '-50%' },
+  exitNext: { left: '90%', opacity: 0, scale: 0.5, x: '-50%', y: '-50%' },
+  initialNext: { left: '90%', opacity: 0, scale: 0.5, x: '-50%', y: '-50%' },
+}
+
+const setVisibleItems = (activeItemValue) => {
+  if (activeItemValue === 1) {
+    return [
+      { value: 1, variant: 'active' }, 
+      { value: 2, variant: 'next' },
+    ];
+  }
+  if (activeItemValue === 8) {
+    return [
+      { value: 7, variant: 'prev' }, 
+      { value: 8, variant: 'active' },
+    ];
+  }
+
+  return [1, 2, 3, 4, 5, 6, 7, 8]
+    .filter(
+      (item) => item >= activeItemValue - 1 && item <= activeItemValue + 1
+    )
+    .map((item, index) => ({
+      value: item,
+      variant: index === 0 ? 'prev' : index === 1 ? 'active' : 'next'
+    }));
+};
 
 const Slider = () => {
-  const items = [1, 2, 3, 4, 5, 6, 7, 8];
-  const [activeItem, setActiveItem] = useState(1);
-  const getVisibleItems = () => {
-    if (activeItem === 1) {
-      return [1, 2, 3];
-    }
-    if (activeItem === 8) {
-      return [6, 7, 8];
-    }
+  const [items, setItems] = useState(setVisibleItems(1));
 
-    return items.filter((item) => item >= activeItem - 1 && item <= activeItem + 1);
-  };
-
-  useSpatialNavigation('#slider .column', [activeItem]);
+  useSpatialNavigation("#slider .Slider__item", [items]);
 
   return (
-    <div id="slider" className="columns">
-      {getVisibleItems().map((item) => {
-        return (
-          <div key={item} className="column" onFocus={() => {
-            setActiveItem(item);
-          }}>
-            <p className="notification is-primary">
-              Item {item}
-            </p>
-          </div>
-        );
-      })}
+    <div id="slider" className="Slider">
+      <AnimatePresence>
+        {items.map(({ value, variant }) => {
+          return (
+            <motion.div
+              key={value}
+              className="Slider__item"
+              onFocus={() => {
+                console.log('onFocus', value)
+                setItems(setVisibleItems(value));
+              }}
+              variants={variants}
+              animate={variant}
+              initial={variant === 'prev' ? 'initialPrev' : 'initialNext'}
+              exit={variant === 'prev' ? 'exitPrev' : 'exitNext'}
+            >
+              <p className={`notification is-primary Slider__content Slider__content--${value}`}>Item {value}</p>
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
     </div>
   );
 };
--- src/hooks/useSpatialNavigation.js
+++ src/hooks/useSpatialNavigation.js
@@ -1,11 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import SpatialNavigation from 'spatial-navigation-js';
 
 export const useSpatialNavigation = (selector, deps = []) => {
+  const [prevId, setPrevId] = useState(null);
   useEffect(() => {
+    if (!!prevId) {
+      SpatialNavigation.remove(prevId);
+    }
     const id = SpatialNavigation.add({
       selector,
     });
+    setPrevId(prevId);
     SpatialNavigation.makeFocusable();
 
     return () => {
