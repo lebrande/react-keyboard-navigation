bbfa00cddfa48fc9aeefb3baf299ebdc51487875
Jakub Pusiak <jakub.pusiak@gmail.com>
2020-09-13 23:48:42 +0200

    Tidy up.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,52 @@
-import React, { useEffect, FC } from 'react';
-import SpatialNavigation from 'spatial-navigation-js';
-import Ribbon from '../Ribbon/Ribbon';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Routing from '../Routing/Routing';
+import React, { FC } from 'react';
+import {
+  Switch,
+  Route,
+  useLocation,
+} from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
+import Channels from '../Channels/Channels';
+import Channel from '../Channel/Channel';
+import VideoPlayer from '../VideoPlayer/VideoPlayer';
+import Page from '../Page/Page';
 
 const App: FC = () => {
-  useEffect(() => {
-    SpatialNavigation.init();
-    SpatialNavigation.focus();
-  }, []);
+  const location = useLocation();
 
   return (
-    <>
-      <Ribbon />
-      <Header />
-      <Routing />
-      <Footer />
-    </>
+    <section className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <AnimatePresence exitBeforeEnter>
+              <Switch location={location} key={location.key}>
+                <Route exact path="/videos/:videoId">
+                  <Page>
+                    <VideoPlayer />
+                  </Page>
+                </Route>
+                <Route exact path="/channels/:channelId">
+                  <Page>
+                    <Channel />
+                  </Page>
+                </Route>
+                <Route exact path="/">
+                  <Page>
+                    <Channels />
+                  </Page>
+                </Route>
+                <Route>
+                  <Page>
+                    <p>Not found 404</p>
+                  </Page>
+                </Route>
+              </Switch>
+            </AnimatePresence>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
